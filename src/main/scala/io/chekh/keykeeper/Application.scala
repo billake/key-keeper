0645f2225a649813fab0ab1fd3f88015e0a2a13e
ArtsiomChekh <qbartem@gmail.com>
2024-11-06 11:16:20 +0100

    Added a simple EmberServer.

--- src/main/scala/io/chekh/keykeeper/Application.scala
+++ src/main/scala/io/chekh/keykeeper/Application.scala
@@ -1,7 +1,13 @@
 package io.chekh.keykeeper
 
 import cats.effect.{IO, IOApp}
+import io.chekh.keykeeper.http.HttpApi
+import org.http4s.ember.server.EmberServerBuilder
 
 object Application extends IOApp.Simple {
-  override def run: IO[Unit] = IO.println("Hello world!")
+  override def run: IO[Unit] = EmberServerBuilder
+    .default[IO]
+    .withHttpApp(HttpApi[IO].endpoints.orNotFound)
+    .build
+    .use(_ => IO.println("Server ready!") *> IO.never)
 }
